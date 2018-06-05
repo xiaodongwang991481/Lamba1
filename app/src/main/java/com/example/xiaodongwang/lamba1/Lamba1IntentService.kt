716c6d2e617d4b2e193feab00a48aedc6ab74f4d
xiaodongwang991481 <xiaodongwang991481@gmail.com>
2018-06-05 15:15:01 -0700

    update

--- app/src/main/java/com/example/xiaodongwang/lamba1/Lamba1IntentService.kt
+++ app/src/main/java/com/example/xiaodongwang/lamba1/Lamba1IntentService.kt
@@ -1,13 +1,13 @@
 package com.example.xiaodongwang.lamba1
 
-import android.app.IntentService
+import android.app.*
 import android.content.Intent
 import android.content.Context
+import android.graphics.Color
 import android.support.v4.app.NotificationCompat
 import android.util.Log
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.app.TaskStackBuilder
+import android.os.Build
+import android.os.IBinder
 
 
 /**
@@ -17,6 +17,52 @@ import android.app.TaskStackBuilder
  * helper methods.
  */
 class Lamba1IntentService : IntentService("Lamba1IntentService") {
+
+    private fun startInForeground() {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+                this, 0, notificationIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notification = Notification.Builder(this)
+                .setContentTitle("Lambda1Service")
+                .setContentText("lambda1 service")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .setTicker("TICKER")
+                .setAutoCancel(false)
+                .setWhen(System.currentTimeMillis())
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .build()
+        startForeground(101, notification)
+    }
+
+    override fun onCreate() {
+        Log.i(LOG_TAG, "create service")
+        super.onCreate()
+        createNotificationChannel()
+        startInForeground()
+    }
+
+    override fun onDestroy() {
+        Log.i(LOG_TAG, "destory service")
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        Log.i(LOG_TAG, "bind service")
+        return super.onBind(intent)
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Log.i(LOG_TAG, "unbind service")
+        return super.onUnbind(intent)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(LOG_TAG, "receive start command")
+        return super.onStartCommand(intent, flags, startId)
+    }
 
     override fun onHandleIntent(intent: Intent?) {
         Log.i(LOG_TAG, "receive message")
@@ -33,16 +79,35 @@ class Lamba1IntentService : IntentService("Lamba1IntentService") {
         var payload = ""
         if (intent.hasExtra("payload")) {
             payload = intent.getStringExtra("payload")
+        } else {
             Log.i(LOG_TAG, "payload is missing in intent")
         }
         val sendIntent = Intent("com.example.xiaodongwang.lambdamaster.resultreceiver")
         sendIntent.setPackage("com.example.xiaodongwang.lambdamaster")
         sendIntent.putExtra("name", name)
         sendIntent.putExtra("payload", payload)
+        Log.i(LOG_TAG, "send broadcast message $name=$payload")
         sendBroadcast(sendIntent)
+    }
+
+    fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            val mChannel = NotificationChannel(
+                    NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_MAX
+            )
+            mChannel.enableLights(true)
+            // Sets the notification light color for notifications posted to this
+            // channel, if the device supports this feature.
+            mChannel.lightColor = Color.RED
+            notificationManager.createNotificationChannel(mChannel)
+        }
     }
 
     companion object {
         private val LOG_TAG = "Lamba1IntentService"
+        private val NOTIFICATION_CHANNEL_ID = "lambda1"
+        private val NOTIFICATION_CHANNEL_NAME = "lambda1"
     }
 }
--- app/src/main/java/com/example/xiaodongwang/lamba1/MainActivity.kt
+++ app/src/main/java/com/example/xiaodongwang/lamba1/MainActivity.kt
@@ -1,21 +1,36 @@
 package com.example.xiaodongwang.lamba1
 
+import android.Manifest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
 
     inner class StartService : View.OnClickListener {
         override fun onClick(v: View?) {
+            if (payload == null || payload.text.isNullOrBlank()) {
+                Log.e(LOG_TAG, "empty payload")
+                Toast.makeText(
+                        this@MainActivity, "payload is empty", Toast.LENGTH_SHORT
+                ).show()
+                return
+            }
             val intent = Intent("com.example.xiaodongwang.lamba1")
             intent.setPackage("com.example.xiaodongwang.lamba1")
+            var uuid = UUID.randomUUID().toString()
+            intent.putExtra("name", uuid)
+            intent.putExtra("payload", payload.text.toString())
             Log.i(LOG_TAG, "start service")
-            startService(intent)
+            if (startForegroundService(intent) == null) {
+                Log.e(LOG_TAG, "failed to start service")
+            }
         }
     }
 
@@ -24,15 +39,35 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent("com.example.xiaodongwang.lamba1")
             intent.setPackage("com.example.xiaodongwang.lamba1")
             Log.i(LOG_TAG, "stop service")
-            stopService(intent)
+            if(!stopService(intent)) {
+                Log.e(LOG_TAG, "failed to stop service")
+            }
         }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(LOG_TAG, "create activity")
+        requestPermissions()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         start_service.setOnClickListener(StartService())
         stop_service.setOnClickListener(StopService())
+    }
+
+    fun requestPermissions() {
+        var permissions = ArrayList<String>()
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            for (permission in arrayOf(
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.INTERNET
+            )) {
+                Log.d("permission", "permission denied to $permission - requesting it")
+                permissions.add(permission)
+
+            }
+            requestPermissions(permissions.toTypedArray(), PERMISSION_REQUEST_CODE)
+        }
     }
 
     /**
@@ -48,5 +83,6 @@ class MainActivity : AppCompatActivity() {
             System.loadLibrary("native-lib")
         }
         private val LOG_TAG = "MainActivity"
+        private val PERMISSION_REQUEST_CODE = 1
     }
 }
